ae0c57c48aed25a02190f36342d99191924419f9
Karl Herrick <me@karlherrick.com>
2018-06-11 21:09:02 -0400

    extend alpine-elixir-node image to install phoenix

--- bin/docker-compose-bash-root-shell.sh
+++ bin/docker-compose-bash-root-shell.sh
@@ -4,4 +4,4 @@ readonly SCRIPT_SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${SCRIPT_SOURCE}/../docker/" || exit 1
 
-docker-compose exec -u root kherrick_alpine-elixir-node bash
+docker-compose exec -u root kherrick_alpine-elixir-node-phoenix bash
--- bin/docker-compose-node-shell.sh
+++ bin/docker-compose-node-shell.sh
@@ -4,4 +4,4 @@ readonly SCRIPT_SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${SCRIPT_SOURCE}/../docker/" || exit 1
 
-docker-compose exec -u user kherrick_alpine-elixir-node node
+docker-compose exec -u user kherrick_alpine-elixir-node-phoenix node
